aa2eeb0b6319d5f382651af7ccac8ee534c71b80
italobarrosme <italobarros.me@gmail.com>
2023-07-06 15:30:52 -0300

    chore: implement fix list trips cards

--- src/app/modules/RecommendedTrips/components/CardTrip/CardTrip.tsx
+++ src/app/modules/RecommendedTrips/components/CardTrip/CardTrip.tsx
@@ -8,22 +8,24 @@ import {ReactCountryFlag} from "react-country-flag"
 export const CardTrip = ({ location, name, startDate, pricePerDay, coverImage, countryCode }: CardTripProps) => {
   console.log(startDate, 'START')
   return (
-    <div className="w-80 flex flex-col gap-2">
-      <div className="w-[280px] h-[280px] mb-2 relative">
+    <div className="w-80 flex flex-col gap-2 justify-center">
+      <div className="w-[280px] h-[280px] mb-2 relative m-auto">
         {coverImage ? <Image src={coverImage} alt={name} fill objectFit="cover" className="rounded-2xl" /> : null}
       </div>
-      <Title label={name} className="text-base" />
-      <div className="text-xs flex flex-col gap-1">
-        <p className="flex items-center gap-2">
-          <span>
-            <ReactCountryFlag countryCode={countryCode} style={{
-              width: '16px',
-              height: '16px',
-            }} svg />
-          </span>
-          {location}
-        </p>
-        <p><span className="text-brand-secondary font-semibold">{FormatCurrencyToBRL(pricePerDay)}</span> por noite</p>
+      <div className="w-[280px] m-auto">
+        <Title label={name} className="text-base" />
+        <div className="text-xs flex flex-col gap-1">
+          <p className="flex items-center gap-2">
+            <span>
+              <ReactCountryFlag countryCode={countryCode} style={{
+                width: '16px',
+                height: '16px',
+              }} svg />
+            </span>
+            {location}
+          </p>
+          <p><span className="text-brand-secondary font-semibold">{FormatCurrencyToBRL(pricePerDay)}</span> por noite</p>
+        </div>
       </div>
     </div>
 
--- src/app/modules/RecommendedTrips/components/ListCardTrips/ListCardTrips.tsx
+++ src/app/modules/RecommendedTrips/components/ListCardTrips/ListCardTrips.tsx
@@ -12,7 +12,7 @@ export const ListCardTrips = ({
 
 
   return (
-    <div className="flex flex-col gap-4 justify-center items-center">
+    <div className="flex flex-col gap-4 justify-center items-center my-4">
       {cardsTrips.map((cardTrip) => (
         <CardTrip key={cardTrip.id} {...cardTrip} />
       ))}
